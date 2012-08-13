448c33731fd3375c07c8796990652c0c70c0ad76
Ondrej Certik <ondrej.certik@gmail.com>
2012-08-12 18:58:07 -0700

    Install python-dev

--- fabfile.py
+++ fabfile.py
@@ -18,7 +18,7 @@ def prepare_apt():
     # This is needed to avoid the EULA dialog
     # (http://askubuntu.com/questions/16225/how-can-i-accept-the-agreement-in-a-terminal-like-for-ttf-mscorefonts-installer)
     sudo("echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections")
-    sudo("apt-get -qq install git wine python-virtualenv texlive-latex-recommended make")
+    sudo("apt-get -qq install git wine python-virtualenv texlive-latex-recommended make python-dev")
 
 def copy():
     run("mkdir -p repos/numpy-vendor")
